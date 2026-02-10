b9e5bfd2d1891df34d7da6e6a90b8a9aa48bc05d
unknown <Bruno de Fraga>
2026-02-10 11:54:06 -0300

    primeiro commit

---
+++ brunofragadev/src/main/java/com/brunofragadev/BrunofragadevApplication.java
@@ -0,0 +1,13 @@
+package com.brunofragadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunofragadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunofragadevApplication.class, args);
+	}
+
+}
---
+++ brunofragadev/src/test/java/com/brunofragadev/BrunofragadevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunofragadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunofragadevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
