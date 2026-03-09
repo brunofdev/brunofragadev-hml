65155fc1535c7967279580dd2076ed51c937e83d
Bruno Fraga <brunofragaa97@gmail.com>
2026-03-09 01:11:00 -0300

    adicionado google client id no property

--- brunofragadev-hml/src/main/java/com/brunofragadev/autenticacao/service/AuthenticationService.java
+++ brunofragadev-hml/src/main/java/com/brunofragadev/autenticacao/service/AuthenticationService.java
@@ -10,7 +10,7 @@ import com.brunofragadev.usuarios.mapper.UsuarioMapeador; // Supondo que você u
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-// 🚨 IMPORTAÇÕES DO GOOGLE
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
