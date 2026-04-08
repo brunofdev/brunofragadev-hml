4ee6d11f8eb55fe5749b01863990d1f601829624
Bruno Fraga <brunofragaa97@gmail.com>
2026-04-08 17:56:03 -0300

    bug: corrigi bug ao atualizar projeto

--- brunofragadev-hml/src/main/java/com/brunofragadev/module/project/application/mapper/ProjectMapper.java
+++ brunofragadev-hml/src/main/java/com/brunofragadev/module/project/application/mapper/ProjectMapper.java
@@ -6,6 +6,7 @@ import com.brunofragadev.module.project.domain.entity.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProjectMapper {
@@ -41,7 +42,7 @@ public class ProjectMapper {
                     step.setText(stepRequest.text());
                     step.setCmd(stepRequest.cmd());
                     return step;
-                }).toList();
+                }).collect(Collectors.toList());
                 setup.setSteps(stepsList);
             }
             project.setSetup(setup);
@@ -56,7 +57,7 @@ public class ProjectMapper {
                 projectImage.setLegenda(imageRequest.legenda());
 
                 return projectImage;
-            }).toList();
+            }).collect(Collectors.toList());
             project.setGaleria(images);
         }
 
